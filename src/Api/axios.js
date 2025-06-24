749592a5a08d73dc572b6dc18655edec4de9e730
Nardos-26 <nardosteshome95@gmail.com>
2025-06-24 19:20:47 -0400

    deploy to netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  //Local instance of firebase functions
+  //Local instance of firebase functionsgi
   // baseURL: "http://127.0.0.1:5001/clone-a1e6b/us-central1/api",
 
   //deployed version of firebase function
